8bfbea9ca424d05b5d7d65e31d071cd194fb783c
Yakup Zengin <clafedersin@gmail.com>
2023-11-12 21:05:16 +0300

    Updated : Test.java

--- LinkedList/DoublyLinkedList/src/Test.java
+++ LinkedList/DoublyLinkedList/src/Test.java
@@ -1,2 +1,17 @@
 public class Test {
+    public static void main(String [] args){
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
+        doublyLinkedList.insertToFront(0);
+        doublyLinkedList.insertToFront(-1);
+        doublyLinkedList.insertToFront(-2);
+        doublyLinkedList.display();
+        System.out.println("**");
+        doublyLinkedList.insertToEnd(1);
+        doublyLinkedList.insertToEnd(2);
+        doublyLinkedList.insertToEnd(3);
+        doublyLinkedList.display();
+        System.out.println("**");
+        doublyLinkedList.delete(3);
+        doublyLinkedList.display();
+    }
 }

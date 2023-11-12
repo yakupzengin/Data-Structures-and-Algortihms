3e8c92d1113264aaf9f780b598028a4f6b397797
Yakup Zengin <clafedersin@gmail.com>
2023-11-12 19:03:34 +0300

    Updated : DNode.java

--- LinkedList/DoublyLinkedList/src/DNode.java
+++ LinkedList/DoublyLinkedList/src/DNode.java
@@ -1,3 +1,12 @@
 public class DNode<T extends Comparable> {
-
+    public T value;
+    public DNode<T> next,prev;
+    public DNode(T val){
+        this.value =val;
+        this.next=null;
+        this.prev = null;
+    }
+    public String toString(){
+        return String.valueOf(value);
+    }
 }

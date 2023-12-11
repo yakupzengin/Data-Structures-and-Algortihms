4540a197d28ea0432925f84eddcff757ebda325a
Yakup Zengin <clafedersin@gmail.com>
2023-12-11 16:39:32 +0300

    check algorithm BubleSort.java

--- BubbleSort/src/BubleSort.java
+++ BubbleSort/src/BubleSort.java
@@ -2,26 +2,25 @@ public class BubleSort {
 
     // This method implements the bubble sort algorithm to sort the given array of integers.
     public static void bubbleSort(int[] values) {
-        boolean sorted = true;
+        boolean sorted = false;
 
         for (int i = 0; i < values.length - 1; i++) {
             for (int j = 0; j < values.length - 1 - i; j++) {
                 if (values[j] > values[j + 1]) {
-                    int temp = values[j];
-                    values[j] = values[j + 1];
-                    values[j + 1] = temp;
-                    sorted = false;
+                    int temp = values[j+1];
+                    values[j+1] = values[j];
+                    values[j ] = temp;
+                    sorted = true;
                 }
             } // inner for loop
-            if (sorted)
-                return;
-            sorted = true;
+            if (!sorted)
+                break;
         } // outer for loop
 
     }
 
     public static void main(String[] args) {
-        int[] myValues = {5, 1, 9, 10, 3, 4, 0};
+        int[] myValues = {5, 1,100,-50,-109, 9, 10, 3, 4, 0};
         bubbleSort(myValues);
 
         // Displaying the sorted array elements after applying the bubble sort algorithm.

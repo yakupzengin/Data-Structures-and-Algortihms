83cd5cc031ef0538d0c282e1dfd34f3e647514f7
Yakup Zengin <clafedersin@gmail.com>
2023-12-17 17:36:27 +0300

    check the method "findHeigt" BinarySearchTree.java

--- Trees/BinarySearchTree/src/Test.java
+++ Trees/BinarySearchTree/src/Test.java
@@ -3,12 +3,11 @@ public class Test {
         BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
 
         // Insert values into the binary search tree
-        binarySearchTree.insert(1);
         binarySearchTree.insert(2);
         binarySearchTree.insert(8);
         binarySearchTree.insert(10);
         binarySearchTree.insert(16);
-        binarySearchTree.insert(7);
+        binarySearchTree.insert(1);
 
         // Perform in-order traversal and print the nodes
         binarySearchTree.inOrder();
@@ -16,5 +15,6 @@ public class Test {
         // Search for a value in the binary search tree and print the result
         System.out.println(binarySearchTree.search(16));
         System.out.println(binarySearchTree.search(101));
+        System.out.println(binarySearchTree.findHeight());
     }
 }

4c3eaecfb2ba20860b681bca524e9fa541bcd82d
Yakup Zengin <clafedersin@gmail.com>
2023-12-21 01:49:35 +0300

    updated Edge class : Edge.java

--- Graph/src/Edge.java
+++ Graph/src/Edge.java
@@ -1,2 +1,9 @@
-public class Edge {
+public class Edge <T extends Comparable>{
+    T vertexId;
+    Edge<T> nextEdge;
+
+    public Edge(T id){
+        vertexId=id;
+        nextEdge=null;
+    }
 }

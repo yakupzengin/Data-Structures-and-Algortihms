09f30fc704f1419aa7ab69462ef6db9507d52e03
Yakup Zengin <clafedersin@gmail.com>
2023-12-21 01:48:28 +0300

    check the methods : Test.java

--- Graph/src/Test.java
+++ Graph/src/Test.java
@@ -6,13 +6,16 @@ public class Test {
         myGraph.addVertex(3);
         myGraph.addVertex(4);
         myGraph.addVertex(5);
+
         myGraph.addEdge(1,3);
         myGraph.addEdge(1,5);
         myGraph.addEdge(3,4);
         myGraph.addEdge(3,5);
         myGraph.addEdge(4,5);
         myGraph.addEdge(2,3);
+
         myGraph.display();
+
         System.out.println(myGraph.outDegree(1));
         System.out.println(myGraph.inDegree(5));
 

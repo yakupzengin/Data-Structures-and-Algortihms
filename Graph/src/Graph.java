13731eb1e42a26532f8f7b2d60e63fd361ed4cf3
Yakup Zengin <clafedersin@gmail.com>
2023-12-21 01:43:48 +0300

    add findVertex method :  Graph.java

--- Graph/src/Graph.java
+++ Graph/src/Graph.java
@@ -1,2 +1,20 @@
-public class Graph {
+public class Graph <T extends Comparable>{
+    Vertex<T> head;
+
+    public Vertex<T> findVertex(T id){
+        Vertex<T> iterator = head;
+        while(iterator !=null){
+            if (iterator.id.compareTo(id) == 0){
+                return iterator;
+            }
+            iterator = iterator.nextVertex;
+        }
+        return null;
+    }
+
+
+
+
+
+
 }

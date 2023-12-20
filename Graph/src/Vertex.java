20082772208de58a1085afbd067a63268ce3301a
Yakup Zengin <clafedersin@gmail.com>
2023-12-21 01:49:21 +0300

    updated Vertex class : Vertex.java

--- Graph/src/Vertex.java
+++ Graph/src/Vertex.java
@@ -1,2 +1,10 @@
-public class Vertex {
+public class Vertex <T extends Comparable>{
+    T id;
+    Vertex<T> nextVertex;
+    Edge<T> edgeLink;
+    public Vertex(T id){
+        this.id=id;
+        nextVertex=null;
+        edgeLink=null;
+    }
 }
